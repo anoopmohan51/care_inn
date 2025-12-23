a21874b7b241584c3e8e9f0ca55bb2847a152b30
ANOOP <anoopmohan51@gmail.com>
2025-12-23 15:09:14 +0530

    pull from release

--- workorder_api/serializers/workorder_follower_serializer.py
+++ workorder_api/serializers/workorder_follower_serializer.py
@@ -7,8 +7,11 @@ class WorkOrderFollowerSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def create(self, validated_data):
         request = self.context.get('request')
+        validated_data['created_user'] = request.user
+        validated_data['tenant'] = request.user.tenant
         return super().create(validated_data)
     
     def update(self, instance, validated_data):
         request = self.context.get('request')
+        validated_data['updated_user'] = request.user
         return super().update(instance, validated_data)
