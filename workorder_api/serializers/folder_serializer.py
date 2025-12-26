445feeee524be239067656f08fc2887c996ddd16
ANOOP <anoopmohan51@gmail.com>
2025-12-26 18:53:37 +0530

    remove table

--- workorder_api/serializers/folder_serializer.py
+++ workorder_api/serializers/folder_serializer.py
@@ -1,21 +1,21 @@
-from rest_framework import serializers
-from workorder_api.models import Folder
+# from rest_framework import serializers
+# from workorder_api.models import Folder
 
-class FolderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Folder
-        fields = '__all__'
-        extra_kwargs = {
-            'created_user': {'read_only': True},
-            'updated_user': {'read_only': True},
-        }
-    def create(self, validated_data):
-        request = self.context.get('request')
-        validated_data['created_user'] = request.user
-        validated_data['tenant'] = request.user.tenant
-        return super().create(validated_data)
+# class FolderSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Folder
+#         fields = '__all__'
+#         extra_kwargs = {
+#             'created_user': {'read_only': True},
+#             'updated_user': {'read_only': True},
+#         }
+#     def create(self, validated_data):
+#         request = self.context.get('request')
+#         validated_data['created_user'] = request.user
+#         validated_data['tenant'] = request.user.tenant
+#         return super().create(validated_data)
     
-    def update(self, instance, validated_data):
-        request = self.context.get('request')
-        validated_data['updated_user'] = request.user
-        return super().update(instance, validated_data)
+#     def update(self, instance, validated_data):
+#         request = self.context.get('request')
+#         validated_data['updated_user'] = request.user
+#         return super().update(instance, validated_data)
