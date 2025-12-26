f6d239147f98f593eb3bd7598a75e6b6378bb7ae
ANOOP <anoopmohan51@gmail.com>
2025-12-26 18:59:04 +0530

    table remove

--- workorder_api/models/folder_informations.py
+++ workorder_api/models/folder_informations.py
@@ -1,7 +1,7 @@
 from django.db import models
-from workorder_api.models.folder import Folder
+# from workorder_api.models.folder import Folder
 from core_api.models.appusers import AppUsers
 
 class FolderInformations(models.Model):
-    folder = models.ForeignKey(Folder, on_delete=models.PROTECT,null=True)
+    # folder = models.ForeignKey(Folder, on_delete=models.PROTECT,null=True)
     information = models.TextField(null=True)
