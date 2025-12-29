0278b18916566ba0b8361be6d4fa13685d82caef
ANOOP <anoopmohan51@gmail.com>
2025-12-29 18:10:15 +0530

    Test migration

--- workorder_api/models/workorder.py
+++ workorder_api/models/workorder.py
@@ -71,6 +71,7 @@ class WorkOrder(models.Model):
     end_date=models.DateTimeField(null=True)
     service = models.ForeignKey(Services, on_delete=models.PROTECT,null=True)
     primary_image = models.ForeignKey(StaticFiles, on_delete=models.PROTECT,null=True)
+    test_field123 = models.CharField(max_length=255,null=True)  
 
 
     class Meta:
