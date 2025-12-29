eb9cff35f9fc18db98ab539b9ef0d4b65504e30c
ANOOP <anoopmohan51@gmail.com>
2025-12-29 10:54:08 +0530

    add testfield

--- workorder_api/models/workorder.py
+++ workorder_api/models/workorder.py
@@ -71,6 +71,7 @@ class WorkOrder(models.Model):
     end_date=models.DateTimeField(null=True)
     service = models.ForeignKey(Services, on_delete=models.PROTECT,null=True)
     primary_image = models.ForeignKey(StaticFiles, on_delete=models.PROTECT,null=True)
+    is_emergency = models.BooleanField(default=False)
 
 
     class Meta:
