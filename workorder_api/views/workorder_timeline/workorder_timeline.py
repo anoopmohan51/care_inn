2280f1a79e2cb261a2c78d4b8d02375ad0145f2d
ANOOP <anoopmohan51@gmail.com>
2026-01-06 20:43:51 +0530

    activity

--- workorder_api/views/workorder_timeline/workorder_timeline.py
+++ workorder_api/views/workorder_timeline/workorder_timeline.py
@@ -5,6 +5,9 @@ from core_api.response_utils.custom_response import CustomResponse
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from ...serializers.workorder_serializer import WorkOrderSerializer
+from datetime import datetime
+from workorder_api.models import WorkOrder
 
 class WorkOrderTimelineCreateView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -12,17 +15,72 @@ class WorkOrderTimelineCreateView(APIView):
     def post(self, request):
         try:
             data = request.data
-            serializer = WorkOrderTimelineSerializer(data=data, context={'request': request})
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
+            activity = data.get('activity')
+            workorder_id = data.get('workorder')
+            user_id = request.user.id
+            if not activity or not workorder_id:
                 return CustomResponse(
-                    data=serializer.data,
-                    status="success",
-                    message=["Work order timeline created successfully"],
-                    status_code=status.HTTP_201_CREATED,
+                    data=None,
+                    status="failed",
+                    message=["Activity and workorder are required"],
+                    status_code=status.HTTP_400_BAD_REQUEST,
                     content_type="application/json"
                 )
+            valid_activities = ['TIMER_START','TIMER_STOP','CLOSED','OPEN']
+            if activity not in valid_activities:
+                return CustomResponse(
+                    data=None,
+                    status="failed",
+                    message=["Invalid activity"],
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content_type="application/json"
+                )
+            
+            workorder = WorkOrder.objects.get(id=data.get('workorder'),is_delete=False)
+            workorder_data = _prepare_workorder_status_for_activity(self,activity)
+            if workorder_data:
+                workorder_serializer = WorkOrderSerializer(
+                    workorder,
+                    data=workorder_data,
+                    context={'request': request},
+                    partial=True
+                )
+                if workorder_serializer.is_valid(raise_exception=True):
+                    workorder_serializer.save()
+                    timeline_data = _perpare_timeline_data(self,data,activity,user_id)
+                    timeline_id = data.get("id")
+                    if timeline_id:
+                        timeline_data['id'] = timeline_id
+                        timeline_serializer = WorkOrderTimelineSerializer(
+                            data=timeline_data,
+                            context={'request': request},
+                            partial=True
+                        )
+                    else:
+                        if  activity in ['TIMER_START','TIMER_STOP']:
+                            timeline_serializer = WorkOrderTimelineSerializer(
+                                data=timeline_data,
+                                context={'request': request}
+                            )
+                    timeline_serializer.is_valid(raise_exception=True)
+                    timeline_serializer.save()
+                    return CustomResponse(
+                        data=timeline_serializer.data,
+                        status="success",
+                        message=["Work order timeline created successfully"],
+                        status_code=status.HTTP_201_CREATED,
+                        content_type="application/json"
+                    )        
+        except WorkOrder.DoesNotExist:
+            return CustomResponse(
+                data=None,
+                status="failed",
+                message=["Work order not found"],
+                status_code=status.HTTP_404_NOT_FOUND,
+                content_type="application/json"
+            )
         except Exception as e:
+            print("error:::::::",e)
             return CustomResponse(
                 data=None,
                 status="failed",
@@ -99,3 +157,21 @@ class WorkOrderTimelineListView(APIView):
                 status_code=status.HTTP_400_BAD_REQUEST,
                 content_type="application/json"
             )
+
+def _perpare_timeline_data(self,data,activity,user_id):
+    timeline_data = data.copy()
+    timeline_data['initiated_by'] = user_id
+    if activity=='TIMER_START':
+        timeline_data['from_date'] = datetime.now()
+    elif activity=='TIMER_STOP':
+        timeline_data['to_date'] = datetime.now()
+    return timeline_data
+
+def _prepare_workorder_status_for_activity(self,activity):
+    status_maping = {
+        "TIMER_START": {"status":WorkOrder.WORKORDER_STATUS_IN_PROGRESS},
+        "TIMER_STOP": {"status":WorkOrder.WORKORDER_STATUS_PAUSED},
+        "CLOSED": {"status":WorkOrder.WORKORDER_STATUS_CLOSED},
+        "OPEN": {"status":WorkOrder.WORKORDER_STATUS_ASSIGNED_NOT_STARTED},
+    }
+    return status_maping.get(activity,None)
