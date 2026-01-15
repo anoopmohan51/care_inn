4b52936558630d3c837fe8c666a7779975de7c12
ANOOP <anoopmohan51@gmail.com>
2026-01-15 13:28:14 +0530

    workorder details changes

--- workorder_api/serializers/workorder_serializer.py
+++ workorder_api/serializers/workorder_serializer.py
@@ -3,9 +3,29 @@ from workorder_api.models import WorkOrder
 from workorder_api.activity_context.activity_context import set_activity_user, clear_activity_user
 from django.contrib.auth import get_user_model
 from workorder_api.models.workorder import WorkOrderImages
+from core_api.models.appusers import AppUsers
+from core_api.models.usergroups import UserGroup
+from django.db.models.functions import Concat
+from django.db.models import Value,F
 
 class WorkOrderSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField('get_images')
+    service_name = serializers.SerializerMethodField('get_service_name')
+    assignee_name = serializers.SerializerMethodField('get_assignee_name')
+
+    def get_assignee_name(self, obj):
+        if obj.assignee_type == 'USER':
+            user = AppUsers.objects.filter(id=obj.user.id,is_delete=False).annotate(
+                name = Concat(F('first_name'), Value(' '), F('last_name'))
+            ).values('name').first()
+            return user.get('name') if user else None
+        elif obj.assignee_type == 'TEAM':
+            user_group = UserGroup.objects.filter(id=obj.user_group.id,is_delete=False).values('name').first()
+            return user_group.get('name') if user_group else None
+        return None
+
+    def get_service_name(self, obj):
+        return obj.service.name if obj.service else None
 
     def get_images(self, obj):
         return WorkOrderImages.objects.filter(workorder=obj.id).values()
