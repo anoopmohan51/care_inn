3252e403fc0bb014d6c5a59ed7844f0e52cb5595
ANOOP <anoopmohan51@gmail.com>
2026-01-16 16:07:29 +0530

    folder list fix

--- workorder_api/views/folder_details/folder_details_query.py
+++ workorder_api/views/folder_details/folder_details_query.py
@@ -23,9 +23,9 @@ def _get_folder_details(folder_id,limit,offset):
                     NULL::timestamp AS created_at,
                     NULL::timestamp AS updated_at,
                     NULL::integer as tenant_id,
-                    null::integer AS created_user_id,
-                    null::integer AS updated_user_id,
-                    f.position ::integer,
+                    NULL::integer AS created_user_id,
+                    NULL::integer AS updated_user_id,
+                    f.position ::integer
                 FROM workorder_api_folder f
                 WHERE f.parent_folder_id = %s
 
@@ -51,7 +51,7 @@ def _get_folder_details(folder_id,limit,offset):
                     s.tenant_id,
                     s.created_user_id,
                     s.updated_user_id,
-                    s.position ::integer,
+                    s.position ::integer
                 FROM workorder_api_services s
                 WHERE s.folder_id = %s
 
@@ -77,7 +77,7 @@ def _get_folder_details(folder_id,limit,offset):
                     r.tenant_id,
                     r.created_user_id,
                     r.updated_user_id,
-                    r.position ::integer,
+                    r.position ::integer
                 FROM workorder_api_requested_items r
                 WHERE r.folder_id = %s
 
@@ -103,7 +103,7 @@ def _get_folder_details(folder_id,limit,offset):
                     i.tenant_id,
                     i.created_user_id,
                     i.updated_user_id,
-                    i.position ::integer,
+                    i.position ::integer
                 FROM workorder_api_informations i
                 WHERE i.folder_id = %s
                 ORDER BY position ASC
