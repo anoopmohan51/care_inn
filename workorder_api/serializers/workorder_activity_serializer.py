48519edf2fa5ae007abc9c5d9c73434604eb7e71
ANOOP <anoopmohan51@gmail.com>
2026-01-16 17:35:52 +0530

    activity fix

--- workorder_api/serializers/workorder_activity_serializer.py
+++ workorder_api/serializers/workorder_activity_serializer.py
@@ -36,7 +36,7 @@ class WorkOrderActivitySerializer(serializers.ModelSerializer):
                 user_group = UserGroup.objects.filter(id=obj.from_value.split('-')[1],is_delete=False).values('name').first()
                 name = user_group.get('name')
             elif obj.from_value.startswith('USER-'):
-                user = AppUsers.objects.filter(id=obj.from_value.split('-')[1],is_delete=False).annotate(
+                user = AppUsers.objects.filter(email=obj.from_value.split('-')[1],is_delete=False).annotate(
                     name = Concat(F('first_name'), Value(' '), F('last_name'))
                 ).values('name').first()
                 name = name.get('name')
@@ -51,7 +51,7 @@ class WorkOrderActivitySerializer(serializers.ModelSerializer):
                 user_group = UserGroup.objects.filter(id=obj.to_value.split('-')[1],is_delete=False).values('name').first()
                 name = user_group.get('name')
             elif obj.to_value.startswith('USER-'):
-                user = AppUsers.objects.filter(id=obj.to_value.split('-')[1],is_delete=False).annotate(
+                user = AppUsers.objects.filter(email=obj.to_value.split('-')[1],is_delete=False).annotate(
                     name = Concat(F('first_name'), Value(' '), F('last_name'))
                 ).values('name').first()
                 name = name.get('name')
