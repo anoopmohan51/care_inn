d0c3ce1215eba665f11ad5f2f7ff68c57c408303
ANOOP <anoopmohan51@gmail.com>
2026-01-20 15:19:00 +0530

    workorder filter fix

--- workorder_api/views/workorder/workorder.py
+++ workorder_api/views/workorder/workorder.py
@@ -201,7 +201,7 @@ class WorkorderFilterView(APIView):
 
     # @has_permission("Workorder", "read")
     def post(self, request):
-        # try:
+        try:
             field_lookup = {
                 "id": "id",
                 "description": "description",
@@ -237,12 +237,11 @@ class WorkorderFilterView(APIView):
                 status_code=status.HTTP_200_OK,
                 content_type="application/json"
             )
-        # except Exception as e:
-        #     print("error in Work order filter fetching:::::::::::",e)
-        #     return CustomResponse(
-        #         data=None,
-        #         status="failed",
-        #         message=["Error in Work order filter fetching"],
-        #         status_code=status.HTTP_400_BAD_REQUEST,
-        #         content_type="application/json"
-        #     )
+        except Exception as e:
+            return CustomResponse(
+                data=None,
+                status="failed",
+                message=["Error in Work order filter fetching"],
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content_type="application/json"
+            )
