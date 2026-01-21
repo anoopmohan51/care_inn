53ff9369f77c076102c57ea526b81af13a3f7e64
ANOOP <anoopmohan51@gmail.com>
2026-01-21 14:48:37 +0530

    item error fix

--- workorder_api/views/requested_items/requested_items.py
+++ workorder_api/views/requested_items/requested_items.py
@@ -82,6 +82,7 @@ class RequestedItemsDetailView(APIView):
                 content_type="application/json"
             )
         except Exception as e:
+            print("error::::::::::::::::",e)
             return CustomResponse(
                 data=None,
                 status="failed",
