72a258dbb3c6e65eb718e856989e252501b4c9d4
ANOOP <anoopmohan51@gmail.com>
2026-01-21 15:24:50 +0530

    folder fix

--- workorder_api/views/workorder_settings/workorder_settings.py
+++ workorder_api/views/workorder_settings/workorder_settings.py
@@ -179,7 +179,7 @@ class WorkOrderSettingsFilterView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        try:
+        # try:
             field_lookup = {
                 "id": "id",
                 "name": "name",
@@ -203,12 +203,12 @@ class WorkOrderSettingsFilterView(APIView):
                 status_code=status.HTTP_200_OK,
                 content_type="application/json"
             )
-        except Exception as e:
-            print("error::::::::::::::::::::::",e)
-            return CustomResponse(
-                data=None,
-                status="failed",
-                message=[f"Error in WorkOrderSettings filter fetching"],
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content_type="application/json"
-            )
+        # except Exception as e:
+        #     print("error::::::::::::::::::::::",e)
+        #     return CustomResponse(
+        #         data=None,
+        #         status="failed",
+        #         message=[f"Error in WorkOrderSettings filter fetching"],
+        #         status_code=status.HTTP_400_BAD_REQUEST,
+        #         content_type="application/json"
+        #     )
