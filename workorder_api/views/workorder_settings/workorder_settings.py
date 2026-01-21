1114bd74e0898df51fc2bf38707c43f67238f2bd
ANOOP <anoopmohan51@gmail.com>
2026-01-21 15:30:13 +0530

    folder fix

--- workorder_api/views/workorder_settings/workorder_settings.py
+++ workorder_api/views/workorder_settings/workorder_settings.py
@@ -186,7 +186,6 @@ class WorkOrderSettingsFilterView(APIView):
                 "icon": "icon",
                 "created_at": "created_at",
                 "updated_at": "updated_at",
-                "_position":"_position"
             }
             global_filter = GlobalFilter(
                 request,
