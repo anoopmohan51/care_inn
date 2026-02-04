7a34bf9111ffdbc582c527c740a759dc309b1392
ANOOP <anoopmohan51@gmail.com>
2026-02-04 11:44:48 +0530

    fix

--- workorder_api/views/workorder_settings/workorder_settings.py
+++ workorder_api/views/workorder_settings/workorder_settings.py
@@ -195,6 +195,7 @@ class WorkOrderSettingsFilterView(APIView):
                 default_sort="created_at"
             )
             queryset, count = global_filter.get_serialized_result(serializer=WorkOrderSettingsListSerializer)
+            print("queryset::::::::::::::::::::",queryset)
             sorder_data = sorted(queryset, key=lambda x: x['position'], reverse=False)
             return CustomResponse(
                 data={
